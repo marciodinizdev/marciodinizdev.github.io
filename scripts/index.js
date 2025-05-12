4b54cae04c5d31d63b48173e787d697ffceb049d
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-05-12 20:32:01 -0300

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,24 @@ window.addEventListener('scroll', function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add('active');
   return navbar.classList.remove('active');
 });
+
+// Seleciona o botão
+const backToTop = document.querySelector('.back-to-top');
+
+// Quando o usuário rolar mais de 300px para baixo, mostra o botão
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 300) {
+    backToTop.classList.add('show');
+  } else {
+    backToTop.classList.remove('show');
+  }
+});
+
+// Scroll suave ao clicar
+backToTop.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
